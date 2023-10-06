cc714bc0019ea439629b00036d1fcd2cbb2cd39a
Pablo Carmona <carmonpa@sheridancollege.ca>
2023-10-06 13:03:10 -0400

    Dice-Assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
 
+
+interface Dice{
+  value: number
+  image: string
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Dice-Assignment1';
+  diceImages: Dice[] = []
+  total: number = 0;
+
+  constructor() {
+    this.rollDice();
+  }
+  rollDice(){
+    this.diceImages = []
+    this.total=0;
+
+    for(let i=0;i<3;i++){
+      const diceValue = this.getRandomDiceValue()
+      const diceImage = `assets/dice/side${diceValue}.png`
+      this.diceImages.push({value:diceValue, image: diceImage})
+      this.total += diceValue
+    }
+  }
+
+  getRandomDiceValue(): number{
+    return Math.floor(Math.random()*6)+1
+  }
 }
